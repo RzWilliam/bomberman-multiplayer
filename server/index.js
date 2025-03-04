e9d38e4b78bfb39e71c1ded22c258ef9822eb7f5
RzWilliam <williamrodriguez0621@gmail.com>
2025-03-04 16:04:53 +0100

    🐛 Fix server bug

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
-import { GameRoom } from '/GameRoom.js';
+import { GameRoom } from './Gameroom.js';
 
 const app = express();
 const server = http.createServer(app);
